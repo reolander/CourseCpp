3b7a9ff8ed4d1adda5420703b988497dc21fdccf
Rohan Mohammad <rohan.bphc@yahoo.com>
2016-09-19 20:16:43 +0530

    Update Assignment 3.cpp

--- Assignment 3.cpp
+++ Assignment 3.cpp
@@ -1,40 +1,42 @@
 
-#include <iostream;
+#include <iostream>
+
+int function pow (int base, unsigned int exponent);
+double sine(double opposite, double hypotenuse);
+
 using namespace std;
 
 int main() {
-
-
+	
 	int twocube = pow(2,3);
 	double sin = sine(3,5);
+	
+	cout << twocube << endl;
+	cout << sin << endl;
+}
 
-
-	//Pow Function.
-	int function pow (int base, unsigned int exponent) {
-		
-		int total = 1;
-		
-		if (exponent == 0)
-		  return 0;
-		else
-		{
-		  for (; exponent > 0; exponent--)
-		     total *= base;  
-		}
-
-		return total;
+//Pow Function.
+int function pow (int base, unsigned int exponent) {
+	
+	int total = 1;
+	if (exponent == 0)
+	  return 0;
+	else
+	{
+	  for (; exponent > 0; exponent--)
+	     total *= base;  
 	}
+	return total;
+}
 
-	//Computing sine of an angle using a funciton.
-	double sine(double opposite, double hypotenuse) {
-
-		return opposite/hypotenuse.
-	}
-
+//Computing sine of an angle using a funciton.
+double sine(double opposite, double hypotenuse) {
+	
+	return opposite/hypotenuse.
 }
 
 
-#include <iostream;
+#include <iostream>
 using namespace std;
 
 int main() {
